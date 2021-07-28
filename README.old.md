e9209d85de9aa756964ff9a233f3bb18a77d55dd
beetlejuice96 <dilanbernbae043@gmail.com>
2021-07-27 21:29:38 -0300

    initial commmit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Pungas
+This is a repo to practice  react js and different tools.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>app</div>
+    </div>
+  );
+}
+
+export default App;
